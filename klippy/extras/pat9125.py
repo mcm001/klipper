b13dd9eaeaa434937bcdbccf1bd81a36c34f1e72
matt <matthew.morley.ca@gmail.com>
2018-12-04 23:40:08 -0800

    PAT9125.py: add read_register to module to return arbitrary registers

--- klippy/extras/pat9125.py
+++ klippy/extras/pat9125.py
@@ -18,6 +18,8 @@ class PAT9125:
         self.gcode = self.printer.lookup_object('gcode')
         self.gcode.register_command(
             'SENSOR_READ_ID', self.cmd_SENSOR_READ_ID)
+    def read_register(self,register_id):
+        return self.i2c.i2c_read(register_id, 1)
     def cmd_SENSOR_READ_ID(self, params):
         pr1 = self.i2c.i2c_read([REG_ID_1], 1)
         pr2 = self.i2c.i2c_read([REG_ID_2], 1)
